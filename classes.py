b5e4ded87b4562d5666dfaf573501751343d7c1c
timofey-davidov <timofeydavidov95@gmail.com>
2023-02-21 01:08:25 +0300

    закончил домашнее задание к уроку 13.2 Режимы доступа

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import csv, pprint
+
 class Item:
     """
     Класс для учета товаров
@@ -6,13 +8,20 @@ class Item:
     discount = 20       # скидка
 
     def __init__(self, name: str, price: (int, float), item_count: int):
-        self.name = name
+
+        # часть кода конструктора, проверяющая, что длина имени товара, вводимая в первый раз, не превышает 10 символов
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception("Длина наименования товара превышает 10 символов.")
+
+        self.__name = name
         self.price = price
         self.item_count = item_count
-        if all([type(self.name) == str, type(self.price) in (int, float), type(self.item_count) == int]):
+        if all([type(self.__name) == str, type(self.price) in (int, float), type(self.item_count) == int]):
             Item.all.append(self)
         else:
-            print(f"Проверьте типы данных вводимых аргументов для объекта с именем '{self.name}'")
+            print(f"Проверьте типы данных вводимых аргументов для объекта с именем '{self.__name}'")
 
     def calculate_total_price(self):
         """
@@ -27,3 +36,28 @@ class Item:
         :return:    (int, float)
         """
         self.price = self.price * (1-self.discount/100)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            raise Exception("Длина наименования товара превышает 10 символов.")
+
+    @classmethod
+    def instantiate_from_csv(cls, path = "database/items.csv"):
+        with open(path) as file:
+            file = csv.DictReader(file)
+            for row in file:
+                item = cls(name=row["name"], price=float(row["price"]), item_count=int(row["quantity"]))
+
+    @staticmethod
+    def is_integer(value):
+        if all([type(value) in (int, float), value % 1 == 0]):
+            return True
+        return False
+
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,10 +1,23 @@
 import pytest
 from classes import Item
 
-def test_Item():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-    item3 = Item(999, "Ноутбук", 20)
+@pytest.fixture
+def item1():
+    return Item("Смартфон", 10000, 20)
+
+@pytest.fixture
+def item2():
+    return Item("Ноутбук", 20000, 5)
+
+@pytest.fixture
+def item3():
+    return Item(999, "Ноутбук", 20)
+
+@pytest.fixture
+def item4():
+    return Item("СуперПуперНоутбук", 20000, 5)
+
+def test_Item(item1, item2):
     assert len(Item.all) == 2
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
@@ -13,4 +26,14 @@ def test_Item():
     assert item1.price == 8000
     assert item2.price == 16000
     assert Item.all[0].price == 8000
+
+def test_instantiate_from_csv():
+    assert len(Item.all) == 2
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 7
+
+def test_is_integer():
+    assert Item.is_integer(5) == True
+    assert Item.is_integer(5.0) == True
+    assert Item.is_integer(7.5) == False
 
